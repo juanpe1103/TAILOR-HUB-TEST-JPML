0f0e8a98b7d95dc687bf1251099744858660dff2
juanpe1103 <juan.molina@edu.uah.es>
2025-10-22 14:27:51 +0200

    SERVER RAILWAYS

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const express = require("express");
-const next = require("next");
-const path = require("path");
+import express from "express";
+import next from "next";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { createApp } from "./api/src/app.ts";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
 
 // FRONT: /web
-const app = next({ dev, dir: path.join(__dirname, "web") });
-const handle = app.getRequestHandler();
+const nextApp = next({ dev, dir: path.join(__dirname, "web") });
+const handle = nextApp.getRequestHandler();
 
-// BACK: /api/src/app.ts
-require("ts-node/register/transpile-only");
-const { createApp } = require("./api/src/app");
-
-app.prepare().then(() => {
+nextApp.prepare().then(() => {
   const server = express();
   server.use("/api", createApp());
   server.all("*", (req, res) => handle(req, res));
